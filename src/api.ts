03e1107361880672b2c85a80db0d554cbe72ed8a
YoUnGi102 <tomasgres2001@gmail.com>
2025-04-29 21:07:27 +0200

    Add auth token to api

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,18 @@
 import axios from 'axios';
+import store from './app/store'; // adjust path to your store
 
 const api = axios.create({
   baseURL: '/api',
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const state = store.getState();
+  const token = state.auth.token;
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
