c5e1092fc6d4da2002ebf34b3a387d7ff990dfaf
YoUnGi102 <tomasgres2001@gmail.com>
2025-04-29 20:42:12 +0200

    Add persistance for Redux

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,34 +2,37 @@ import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import store from './store';
+import store, { persistor } from './store';
 import IngredientsPage from '../features/ingredients/ingredientsPage';
 import MainPage from '../components/pages/mainPage';
 import ProtectedRoute from '../features/auth/ProtectedRoute';
 import LoginForm from '../features/auth/LoginForm';
 import './App.css'
+import { PersistGate } from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Router>
-        <div>
-          <h1 className={'title'}>MealPrepTracker</h1>
-          <Routes>
-            <Route path='/login' element={<LoginForm/>}/>
-            <Route
-              path="/ingredients"
-              element={
-                <ProtectedRoute>
-                  <IngredientsPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/" element={<MainPage />} />
-          </Routes>
-        </div>
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <div>
+            <h1 className={'title'}>MealPrepTracker</h1>
+            <Routes>
+              <Route path='/login' element={<LoginForm/>}/>
+              <Route
+                path="/ingredients"
+                element={
+                  <ProtectedRoute>
+                    <IngredientsPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/" element={<MainPage />} />
+            </Routes>
+          </div>
+        </Router>
       <ToastContainer position="top-right" autoClose={3000} />
+      </PersistGate>
     </Provider>
   );
 };
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import ingredientsReducer from '../features/ingredients/ingredientsSlice';
 import authReducer from '../features/auth/authSlice'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  ingredients: ingredientsReducer,
+  auth: authReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 const store = configureStore({
-  reducer: {
-    ingredients: ingredientsReducer,
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production',
 });
 
+
+export const persistor = persistStore(store);
 export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
