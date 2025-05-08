b2cadb607e2df33d7b65893c963d658f334985bd
YoUnGi102 <tomasgres2001@gmail.com>
2025-05-08 14:08:16 +0200

    Add Navbar for small devices

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import styles from './Navbar.module.scss';
 import Logo from '@/components/common/Logo';
 import NavbarItem from './NavbarItem';
 import NavbarSubItem from './NavbarSubItem';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBook, faUser, faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { faBook, faUser, faUtensils, faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className={styles.navbar}>
       <Logo className={styles.logo} />
-      <div className={styles.navbarContainer}>
+      
+      <FontAwesomeIcon
+        icon={faBars}
+        className={styles.hamburger}
+        onClick={() => setIsOpen(!isOpen)}
+      />
+
+      <div
+        className={`${styles.navbarContainer} ${isOpen ? styles.active : ''}`}
+      >
         <NavbarItem link={''} icon={faBook} text={'Recipes'} />
         <NavbarItem link={''} icon={faUtensils} text={'Fridge'} />
         <NavbarItem link={''} icon={faUser} text={'Account'}>
